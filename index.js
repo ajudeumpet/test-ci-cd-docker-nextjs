d6d802a0721d9e507bb5dd3c355afb348ef36fbf
Henrique Rodrigues <henryqrm@gmail.com>
2019-07-21 20:37:47 -0300

    asd

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 9000
 
-app.get('/', (req, res) => res.send('aup!'))
+app.get('/', (req, res) => res.send('asdasdd!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
