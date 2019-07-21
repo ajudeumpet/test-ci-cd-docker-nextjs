873f43e7215d463d50fd63e7b4c6b6f434a2636d
Henrique Rodrigues <henryqrm@gmail.com>
2019-07-21 19:33:23 -0300

    update

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 9000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('aup!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
